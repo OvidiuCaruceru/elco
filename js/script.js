cd9b9fb12a93497cd728b96d2d817585d92fdbbc
OvidiuCaruceru <caruceruovidiu@yahoo.com>
2022-06-14 01:03:23 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//.header .nav menu open and close
+/////////////////////////////////////////////////// .header .nav menu open and close ///////////////////////////////////////////////////
 const menu = document.getElementById('menu');
 const close = document.getElementById('close');
 const nav = document.getElementById('nav');
@@ -10,3 +10,18 @@ menu.addEventListener('click', () => {
 close.addEventListener('click', () => {
     nav.classList.remove('nav--open');
 });
+
+/////////////////////////////////////////////////// .slideshow functionality ///////////////////////////////////////////////////
+const slideshow = document.querySelector('.slideshow__images');
+const slides = Array.from(slideshow.children);
+const prevButton = slideshow.querySelector('.slideshow__button--prev');
+const nextButton = slideshow.querySelector('.slideshow__button--next');
+const dotsContainer = slideshow.querySelector('slideshow__dots');
+const dotsArray = slideshow.getElementsByClassName('slideshow__dot');
+
+//move each slide to the right of previous slide to form a row of images
+const chainSlides = (slide, index) => {
+    slide.style.left = slides[index].getBoundingClientRect().width * index + 'px';
+};
+
+slides.forEach(chainSlides);
